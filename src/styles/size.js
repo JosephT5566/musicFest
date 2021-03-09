0044945742911dd5744b7e94be8f66e3a0e62bcd
Your JosephTseng <zxp930110@hotmail.com.tw>
2021-03-09 18:56:46 +0800

    set general size, set static value

--- src/styles/size.js
+++ src/styles/size.js
@@ -1,4 +1,5 @@
 export const size = {
 	navWidth: '15em',
 	headerHeight: '5em',
+	tableHeadHeight: '2em',
 };
---
+++ src/utils/static.js
@@ -0,0 +1,3 @@
+export const MEGA_START_TIME = '2021-03-27T11:40';
+
+export const MIN = 60000; // 60 sec * 1000 milliseconds
