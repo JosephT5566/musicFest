5077653fb52404311e52e4ec46df3402fd406f6d
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-03-25 22:49:14 +0800

    remove develop code

--- src/utils/static.js
+++ src/utils/static.js
@@ -1,10 +1,6 @@
 export const MEGA_START_TIME = { 0: '2021-03-27T11:40', 1: '2021-03-28T11:40' };
 
-export const MEGA_END_TIME = { 0: '2021-03-27T21:50', 1: '2021-03-28T21:50' };
-
-export const MEGA_START_TIME_TEST = { 0: '2021-03-24T11:40', 1: '2021-03-25T11:40' };
-
-export const MEGA_END_TIME_TEST = { 0: '2021-03-24T21:50', 1: '2021-03-25T21:50' };
+export const MEGA_END_TIME = { 0: '2021-03-27T22:00', 1: '2021-03-28T22:00' };
 
 export const MIN = 60000; // 60 sec * 1000 milliseconds
 
