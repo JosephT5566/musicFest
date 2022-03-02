95a27b18d7ad79f9369a33768ebfd9270349a110
Joseph Tseng <zxp930110@hotmail.com.tw>
2022-03-02 21:21:19 +0800

    move typography to layout

--- nextjs/src/styles/theme.ts
+++ nextjs/src/styles/theme.ts
@@ -1,19 +1,20 @@
 import { createTheme } from '@mui/material/styles';
+import { Typography } from '@mui/material/styles/createTypography';
 
 import { palette } from './palette';
 import { typography } from './typography';
+import { ILayout, layout } from './layout';
 
-declare module '@mui/material/styles/createTypography' {
-	interface TypographyOptions {
-		navWidth?: string;
-		headerHeight?: string;
-		tableHeadHeight?: string;
-		tableHeadMarginBottom?: string;
-		letterSpacing?: string;
+declare module '@mui/material/styles/createTheme' {
+	interface Theme {
+		layout: ILayout;
+	}
+	interface ThemeOptions {
+		layout?: ILayout;
 	}
 }
 
 // Create a theme instance.
-const theme = createTheme({ palette, typography });
+const theme = createTheme({ palette, typography, layout });
 
 export default theme;
