70cf5867148cb366b8d2bd3d4a8c2d78e2e62515
Joseph Tseng <zxp930110@hotmail.com.tw>
2022-03-03 00:57:11 +0800

    set document to tsx

--- nextjs/pages/_document.tsx
+++ nextjs/pages/_document.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/styles';
 import theme from 'styles/theme';
 
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
 		);
 	}
 
-	static async getInitialProps(ctx) {
+	static async getInitialProps(ctx: DocumentContext) {
 		// Render app and page and get the context of the page with collected side effects.
 		const sheets = new ServerStyleSheets();
 		const originalRenderPage = ctx.renderPage;
