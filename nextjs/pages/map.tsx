c35d58f0654b9b8fd587d105b6ee2c116068d939
Your JosephTseng <zxp930110@hotmail.com.tw>
2022-03-03 18:33:37 +0800

    set map page to styled

--- nextjs/pages/map.tsx
+++ nextjs/pages/map.tsx
@@ -2,26 +2,23 @@ import React from 'react';
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 
-const useStyle = makeStyles((theme) => ({
-	mapPage: {
-		overflow: 'scroll',
+const StyledmapPage = styled(Container)({
+	overflow: 'scroll',
+});
+
+const StyledmapImg = styled('img')(({ theme }) => ({
+	[theme.breakpoints.up('md')]: {
+		width: '100%',
 	},
-	mapImg: {
-		[theme.breakpoints.up('md')]: {
-			width: '100%',
-		},
-		[theme.breakpoints.down('sm')]: {
-			width: '200%',
-		},
+	[theme.breakpoints.down('sm')]: {
+		width: '200%',
 	},
 }));
 
 export default function Map() {
-	const classes = useStyle();
-
 	return (
-		<Container className={classes.mapPage}>
-			<img className={classes.mapImg} src={'/megaport_map.jpg'} alt="mega map" />
-		</Container>
+		<StyledmapPage>
+			<StyledmapImg src={'/megaport_map.jpg'} alt="mega map" />
+		</StyledmapPage>
 	);
 }
