e387024aef8ca64a259bb7d2a81886dfd640b11f
Your JosephTseng <zxp930110@hotmail.com.tw>
2022-03-07 17:43:32 +0800

    add Container components

---
+++ nextjs/src/components/base/Container.tsx
@@ -0,0 +1,15 @@
+import { styled } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+
+export const ContentContainer = styled(Container)(({ theme }) => ({
+	minHeight: `calc(100vh - ${theme.layout.header.height}`,
+}));
+
+export const PageContainer = styled('div')(() => ({
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	position: 'relative',
+	gap: '1rem',
+	paddingBlock: '1rem 2rem',
+}));
