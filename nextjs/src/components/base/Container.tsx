1234191273fce94e06ce54cd449aeeda64fb6fe5
Joseph Tseng <zxp930110@hotmail.com.tw>
2022-03-07 22:49:48 +0800

    fix css error

--- nextjs/src/components/base/Container.tsx
+++ nextjs/src/components/base/Container.tsx
@@ -2,7 +2,7 @@ import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 
 export const ContentContainer = styled(Container)(({ theme }) => ({
-	minHeight: `calc(100vh - ${theme.layout.header.height}`,
+	minHeight: `calc(100vh - ${theme.layout.header.height})`,
 }));
 
 export const PageContainer = styled('div')(() => ({
