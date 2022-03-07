125f0fbb10eff15fb2e786f9ce93625fbba4a9d4
Joseph Tseng <zxp930110@hotmail.com.tw>
2022-03-08 02:21:04 +0800

    remove time line page, modify route value

--- nextjs/src/constants/static.ts
+++ nextjs/src/constants/static.ts
@@ -1,6 +1,6 @@
 export const MIN = 60000; // 60 sec * 1000 milliseconds
 
-export const APP_NAME = '大港開唱選擇器';
+export const APP_NAME = '大港開唱看團選擇器';
 
 export const SCALE_UNIT = 1.3;
 
@@ -14,13 +14,11 @@ export const ROUTE = {
 	root: '/',
 	megaport2021: {
 		root: '/megaport2021',
-		timeline: '/megaport2021/timeline',
 		links: '/megaport2021/links',
 		map: '/megaport2021/map',
 	},
 	megaport2022: {
 		root: '/megaport2022',
-		timeline: '/megaport2022/timeline',
 		links: '/megaport2022/links',
 		map: '/megaport2022/map',
 	},
