fffe2579e9a538f1f8bb28ca272c91bca46c1876
Your JosephTseng <zxp930110@hotmail.com.tw>
2022-03-23 16:54:15 +0800

    use Button instead of button, and modify style

--- src/view/payment/TimeTable/StageColumn.tsx
+++ src/view/payment/TimeTable/StageColumn.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useGetSelectedShow, useSelectShow } from 'providers/ShowsProvider';
-import { SimplePaletteColorOptions } from '@mui/material/styles/createPalette';
-
+import moment, { Moment } from 'moment';
 import { styled } from '@mui/material/styles';
 
-import { palette } from 'styles/palette';
+import { SimplePaletteColorOptions } from '@mui/material/styles/createPalette';
+import Button from '@mui/material/Button';
+
+import { useGetSelectedShow, useSelectShow } from 'providers/ShowsProvider';
 import { SCALE_UNIT } from 'constants/static';
-import moment, { Moment } from 'moment';
 import { IArtist, IStage } from 'types/show';
-import { height } from '@mui/system';
 
 const ColumnContainer = styled('div')(({ theme }) => ({
 	textAlign: 'center',
@@ -30,7 +29,7 @@ const Styledhead = styled('div')(({ theme }) => ({
 	letterSpacing: theme.layout.letterSpacing,
 }));
 
-const StyledShowButton = styled('button', {
+const StyledShowButton = styled(Button, {
 	shouldForwardProp: (prop) => prop !== 'background' && prop !== 'height',
 })<{
 	background: string;
@@ -42,6 +41,8 @@ const StyledShowButton = styled('button', {
 	borderRadius: '0.5em',
 	border: 'none',
 	letterSpacing: theme.layout.letterSpacing,
+	lineHeight: 'inherit',
+	textTransform: 'inherit',
 	background: theme.palette.background.paper,
 	color: theme.palette.text.primary,
 	'&.active': {
@@ -57,10 +58,8 @@ const StyledShowButton = styled('button', {
 	[theme.breakpoints.down('md')]: {
 		letterSpacing: '0',
 	},
-	[theme.breakpoints.down('sm')]: {
-		fontSize: '12px',
-	},
 }));
+
 const StyledFreeTimeScale = styled('div')(({ theme }) => ({
 	position: 'relative',
 	height: `${SCALE_UNIT}rem`,
