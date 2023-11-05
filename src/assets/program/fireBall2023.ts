96ae54c95ff32c7eb2b6a582ebc0ccccd0edce82
JosephTseng <zxp930110@hotmail.com.tw>
2023-11-05 13:35:46 +0800

    set up day2 timetable

--- src/assets/program/fireBall2023.ts
+++ src/assets/program/fireBall2023.ts
@@ -125,8 +125,141 @@ const day1: IPerfDay = {
 	],
 };
 
+const day2: IPerfDay = {
+	date: '2023-11-26',
+	dayStartTime: '11:10',
+	dayEndTime: '21:30',
+	stages: [
+		{
+			name: '火舞台',
+			artists: [
+				new IArtist({
+					name: '鄭宜農',
+					startTime: '11:10',
+					endTime: '11:50',
+				}),
+				new IArtist({
+					name: '大象體操',
+					startTime: '13:00',
+					endTime: '13:40',
+				}),
+				new IArtist({
+					name: 'JADE',
+					startTime: '14:55',
+					endTime: '15:35',
+				}),
+				new IArtist({
+					name: '勸世三姊妹',
+					startTime: '16:45',
+					endTime: '17:25',
+				}),
+				new IArtist({
+					name: '國蛋GorDoN',
+					startTime: '18:40',
+					endTime: '19:20',
+				}),
+				new IArtist({
+					name: 'Fire EX. - On Fire Day ft. 馬克SAVAGE.M、阿跨面',
+					startTime: '20:30',
+					endTime: '21:30',
+				}),
+			],
+		},
+		{
+			name: '球舞台',
+			artists: [
+				new IArtist({
+					name: 'TOTALFAT (JP)',
+					startTime: '12:05',
+					endTime: '12:45',
+				}),
+				new IArtist({
+					name: '樂天女孩',
+					startTime: '13:50',
+					endTime: '14:00',
+				}),
+				new IArtist({
+					name: 'TRASH',
+					startTime: '14:00',
+					endTime: '14:40',
+				}),
+				new IArtist({
+					name: 'Silica Gel (KR)',
+					startTime: '15:50',
+					endTime: '16:30',
+				}),
+				new IArtist({
+					name: '美秀集團 ft.浣熊俠',
+					startTime: '17:40',
+					endTime: '18:20',
+				}),
+				new IArtist({
+					name: '樂天女孩',
+					startTime: '18:30',
+					endTime: '18:40',
+				}),
+				new IArtist({
+					name: 'The BONEZ (JP)',
+					startTime: '19:35',
+					endTime: '20:15',
+				}),
+			],
+		},
+		{
+			name: '祭舞台',
+			artists: [
+				new IArtist({
+					name: '夕陽武士',
+					startTime: '11:25',
+					endTime: '11:55',
+				}),
+				new IArtist({
+					name: 'SHOOTUP',
+					startTime: '12:30',
+					endTime: '13:00',
+				}),
+				new IArtist({
+					name: 'Robot Swing',
+					startTime: '13:35',
+					endTime: '14:05',
+				}),
+				new IArtist({
+					name: 'EmptyORio',
+					startTime: '15:15',
+					endTime: '15:55',
+				}),
+				new IArtist({
+					name: '周自從',
+					startTime: '16:15',
+					endTime: '16:45',
+				}),
+				new IArtist({
+					name: '胖虎',
+					startTime: '17:20',
+					endTime: '17:50',
+				}),
+				new IArtist({
+					name: '百合花',
+					startTime: '17:55',
+					endTime: '18:25',
+				}),
+				new IArtist({
+					name: 'HAOTING',
+					startTime: '19:00',
+					endTime: '19:30',
+				}),
+				new IArtist({
+					name: 'DJ MR.GIN',
+					startTime: '20:00',
+					endTime: '20:30',
+				}),
+			],
+		},
+	],
+};
+
 const programList: IProgramList = {
-	perfDays: [formatDate(setArtistId(day1))],
+	perfDays: [formatDate(setArtistId(day1)), formatDate(setArtistId(day2))],
 };
 
 export default programList;
