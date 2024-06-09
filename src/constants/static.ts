1d4f587183a5442a91c75861e62fbae738aa7e51
JosephTseng <zxp930110@hotmail.com.tw>
2024-06-09 13:23:10 +0800

    set consts

--- src/constants/static.ts
+++ src/constants/static.ts
@@ -1,17 +1,47 @@
 export const MIN = 60000; // 60 sec * 1000 milliseconds
 
-export const APP_NAME = '大港開唱選擇器';
+export const APP_NAME = 'Music Festival Timetable Selector';
 
 export const SCALE_UNIT = 1.3;
 
 export const STORAGE_KEY = {
 	day: 'day',
-	notification: 'notification'
+	notification: 'notification',
+};
+
+export const FEST_NAME = {
+	MEGAPORT: 'Megaport',
+	FUJI_ROCK: 'Fuji Rock',
 };
 
 export const ROUTE = {
 	root: '/',
 	links: '/links',
+	megaport: {
+		index: {
+			root: '/megaport',
+		},
+		2021: {
+			root: '/megaport/2021',
+			map: '/megaport2021/map',
+		},
+		2022: {
+			root: '/megaport/2022',
+			map: '/megaport2022/map',
+		},
+		2024: {
+			root: '/megaport/2024',
+			map: '/megaport2024/map',
+		},
+	},
+	fujirock: {
+		index: {
+			root: '/fujirock',
+		},
+		2024: {
+			root: '/fujirock/2024',
+		},
+	},
 	megaport2021: {
 		root: '/megaport2021',
 		map: '/megaport2021/map',
