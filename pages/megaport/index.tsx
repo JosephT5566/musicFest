0454de4e2cc028f098ffbd0b4ecc14be47e7d590
JosephTseng <zxp930110@hotmail.com.tw>
2024-06-10 11:39:47 +0800

    Create PosterContainer component

--- pages/megaport/index.tsx
+++ pages/megaport/index.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 
-import { PageContainer } from 'components/base/Container';
+import { PageContainer, PostersContainer } from 'components/base/Container';
 
 import { APP_NAME, ROUTE, FEST_NAME } from 'constants/static';
 import Head from 'next/head';
 import { H1 } from 'components/base/Typography';
 import { IMAGES } from 'constants/images';
 import PosterCard from 'components/shared/PosterCard';
-
-const PostersContainer = styled('div')(({ theme }) => ({
-	width: '100%',
-	display: 'grid',
-	[theme.breakpoints.down('sm')]: {
-		gridTemplateColumns: 'repeat(2, 1fr)',
-		gap: '1rem',
-		paddingInline: '1rem',
-	},
-	gridTemplateColumns: 'repeat(auto-fill, minmax(15em, 1fr))',
-	gap: '2rem',
-	justifyItems: 'center',
-}));
 
 const pageTitle = `${APP_NAME} - ${FEST_NAME.MEGAPORT}`;
 
--- src/components/base/Container.tsx
+++ src/components/base/Container.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 export const ContentContainer = styled('div')(({ theme }) => ({
 	minHeight: `calc(100vh - ${theme.layout.header.height})`,
@@ -24,6 +26,22 @@ export const PageContainer = styled(Container)(({ theme }) => ({
 export const SelectPageContainer = styled(PageContainer)(({ theme }) => ({
 	maxHeight: `calc(100vh - ${theme.layout.header.height})`,
 }));
+
+export const PostersContainer = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<Box
+			className='poster-container'
+			display="grid"
+			width="100%"
+			gap={['1rem', '2rem']}
+			justifyItems="center"
+			gridTemplateColumns={['repeat(2, 1fr)', 'repeat(auto-fill, minmax(15em, 1fr))']}
+			paddingX={['1rem', undefined]}
+		>
+			{children}
+		</Box>
+	);
+};
 
 export const FixedButtonsContainer = styled('div')({
 	display: 'flex',
