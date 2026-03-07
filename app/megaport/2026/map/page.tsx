c392dfbaab9ffa6b735faf56b65d066406c7387c
JosephTseng <zxp930110@hotmail.com.tw>
2026-03-07 14:24:09 +0800

    install photoswipe for the large image

--- app/megaport/2026/map/page.tsx
+++ app/megaport/2026/map/page.tsx
@@ -1,19 +1,24 @@
 'use client';
 import React from 'react';
-import Map from 'components/Map';
+import { Gallery, Item } from 'react-photoswipe-gallery';
+import 'photoswipe/dist/photoswipe.css';
 
-import { ROUTE, FEST_NAME } from 'constants/static';
-import { IMAGES } from 'constants/images';
+import { PageContainer } from 'components/base/Container';
 
-const title = `${FEST_NAME.MEGAPORT} 2026 - Map`;
+const mapUrl = 'https://cdn.josephtseng-tw.com/megaport2026/map.png';
 
 const Megaport2026Map = () => {
-    return (
-        <Map
-            pageRoutes={ROUTE.megaport[2026]}
-            imageSrc={IMAGES.maps[2025]}
-        />
-    );
+	return (
+		<Gallery>
+			<div className="mt-4 p-2">
+				<Item original={mapUrl} width="1875" height="2560">
+					{({ ref, open }) => (
+						<img className='rounded-md' ref={ref} onClick={open} src={mapUrl} alt="Megaport 2026 Map" />
+					)}
+				</Item>
+			</div>
+		</Gallery>
+	);
 };
 
 export default Megaport2026Map;
